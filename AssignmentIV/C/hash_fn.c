c38eca63feda2ae6ce50a4d7c506e76f9f738bd8
jingting0326 <99161tina@gmail.com>
2025-11-13 19:30:11 +0800

    完成hash_fn.c中的整數並填寫觀察結果

--- AssignmentIV/C/hash_fn.c
+++ AssignmentIV/C/hash_fn.c
@@ -10,7 +10,7 @@
  * @param   key   要進行hash的整數
  * @param   m     表格大小 
  * @return  int   計算出的 Hash 索引
- * 如果 m <= 0，回傳 -1 表示錯誤。
+ * 如果 m <= 0，回傳 -1 表示錯誤
  */
 int myHashInt(int key, int m) {
    if (m <= 0) {
@@ -24,9 +24,32 @@ int myHashInt(int key, int m) {
 
     return index;
 }
-
+/**
+ * @brief   計算一個字串 key 的 hash 索引
+ * @details 使用 Summation Method
+ * @param   key   要進行hash的字串
+ * @param   m     表格大小
+ * @return  int   計算出的 Hash 索引
+ * 如果 m <= 0 或 key 為 NULL，回傳 -1
+ */
 int myHashString(const char* str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
-    return (int)(hash % m); // basic division method
+    if (m <= 0) {
+        printf("Error\n");
+        return -1;
+    }
+    if (str == NULL) {
+        printf("Error\n");
+        return -1;
+    }
+    int i=0;
+    while(str[i]!='\0'){
+        hash+=str[i];
+        i++;
+    }
+    int index=(int)(hash % m);
+    if(index<0){
+        index=(index+m)%m;
+    }
+    return (int)(hash % m); 
 }
